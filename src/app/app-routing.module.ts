08ee0d4d90f7ac812764950753510966c3cb23d3
VinothJV10 <vinothjv10@gmail.com>
2022-03-23 21:35:53 +0530

    centers added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CentersComponent } from './centers/centers.component';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ContentComponent
+  },
+  {
+    path: 'center', component: CentersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/centers/centers.component.ts
+++ src/app/centers/centers.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CentersComponent implements OnInit {
 
-  show: any;
-
+  show_1: any;
+  show_2: any;
+  show_3: any;
+  show_4: any;
 
   constructor() { }
 
